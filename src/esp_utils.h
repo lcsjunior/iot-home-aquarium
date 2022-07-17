cbfeb5aed4c3aba1542e34e3481182a56498bd5d
Luiz Carlos Jr <lcsjuniorsi@gmail.com>
2022-07-17 18:39:25 -0300

    security

--- src/esp_utils.h
+++ src/esp_utils.h
@@ -11,7 +11,7 @@
 
 static const char *ssid PROGMEM = SECRET_SSID;
 static const char *pass PROGMEM = SECRET_PASS;
-static const char *otaPass PROGMEM = SECRET_OTA_PASS;
+static const char *otaPass PROGMEM = ENV_OTA_AUTH;
 static const char *hostname PROGMEM = ENV_HOSTNAME;
 static const char *myTZ PROGMEM = "<-03>3";
 static const char *ntpServer PROGMEM = "pool.ntp.org";
